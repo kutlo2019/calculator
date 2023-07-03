e8203cd0f9c253ca474207aaaa637bc16c40098f
kutlo2019 <kutlopopo@rocketmail.com>
2023-07-03 13:25:10 +0200

    add decimal capability

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const numberButtons = document.querySelectorAll('.number-button');
 
 const operations = document.querySelectorAll('.operation');
 const equals = document.querySelector('.equals');
+const decimal = document.querySelector('.decimal');
 
 const screenTop = document.querySelector('.top-line');
 const screenBottom = document.querySelector('.bottom-line');
@@ -53,6 +54,10 @@ equals.addEventListener('click', e => {
     solution = operate(operation, num1, num2);
     screenTop.innerHTML = solution;
     screenBottom.innerHTML = solution;
+});
+
+decimal.addEventListener('click', e => {
+    populateDisplay(e, screenTop)
 });
 
 function populateDisplay(e, node) {
